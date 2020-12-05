2679f5d588746f963ec5ad12cb6dd3e010685705
Lucas Loezer <loezer.lucas@gmail.com>
2020-12-05 13:03:47 -0300

    titles

--- helloworld.py
+++ helloworld.py
@@ -24,25 +24,25 @@ option = st.selectbox(
 if option == "Sul":
     st.write("Carregando dados do Sul..")
     df = pd.read_csv("Relatorio_Estados_pr-rs-sc.csv", sep=';')
-    st.write("carregado!")
+    st.write("carregado!\n")
 elif option == "Paraná":
     st.write("Carregando dados do Paraná..")
     df = pd.read_csv("Relatorio_pr.csv", sep=';')
-    st.write("carregado!")
+    st.write("carregado!\n")
 elif option == "Santa Catarina":
     st.write("Carregando dados de Santa Catarina..")
     df = pd.read_csv("Relatorio_sc.csv", sep=';')
-    st.write("carregado!")
+    st.write("carregado!\n")
 elif option == "Rio Grande do Sul":
     st.write("Carregando dados do Rio Grande do Sul..")
     df = pd.read_csv("Relatorio_rs.csv", sep=';')
-    st.write("carregado!")
+    st.write("carregado!\n")
 
 df.replace(np.nan, 0, inplace=True)
 df['Data'] = pd.to_datetime(df['Data'], format="%Y-%m-%d")
 
 
-st.write("Deseja filtrar por algum período específico?")
+st.subheader("Deseja filtrar por algum período específico?")
 d3 = st.date_input("Período selecionado", [])
 if len(d3) > 0:
     df = df[df['Data'] >= str(d3[0])]
@@ -67,5 +67,5 @@ for c in df.columns.values:
 df.drop(drop_columns, axis=1, inplace=True)
 
 df.set_index('Data', inplace=True)
-st.subheader("Gráfico")
+st.subheader("Gráfico gerado")
 st.area_chart(df[plot_columns])
