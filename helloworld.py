907b2255b0639a31e2f09b80deb83f164bb89fe2
Lucas Loezer <loezer.lucas@gmail.com>
2020-12-05 13:30:10 -0300

    grafico positividade e fitlro de idade

--- helloworld.py
+++ helloworld.py
@@ -3,13 +3,28 @@ import streamlit as st
 import numpy as np
 import altair as alt
 
+FAIXA_ETARIA = {
+    "0 a 4 anos": [False, 'pcr-positivo-0a4'],
+    "5 a 9 anos": [False, 'pcr-positivo-5a9'],
+    "10 a 14 anos": [False, 'pcr-positivo-10a14'],
+    "15 a 19 anos": [False, 'pcr-positivo-15a19'],
+    "20 a 29 anos": [False, 'pcr-positivo-20a29'],
+    "30 a 39 anos": [False, 'pcr-positivo-30a39'],
+    "40 a 49 anos": [False, 'pcr-positivo-40a49'],
+    "50 a 59 anos": [False, 'pcr-positivo-50a59'],
+    "60 a 69 anos": [False, 'pcr-positivo-60a69'],
+    "70 a 79 anos": [False, 'pcr-positivo-70a79'],
+    "Maior que 80 anos": [False, 'pcr-positivo-80a999']
+}
+
 st.title("[e-SUS Notifica] PCR-Positivo Sul do Brasil")
 st.write("""
 Esse relatório consiste em mostrar os testes positivos para covid-19 listados na API [e-SUS Notifica].
 As informações contidas no relatório são:
 - Quantidade de RT-PCR positivos por dia
 - Quantidade de RT-PCR negativos por dia.
-- Quantidade de RT-PCR positivos nas faixas etárias: [0, 4], [5, 9], [10, 14], [15, 19] [20, 29], [30, 39], [40, 49], [50, 59] [60, 69], [70, 79], [80, 999].
+- Quantidade de RT-PCR positivos nas faixas etárias: [0, 4], [5, 9], [10, 14], [15, 19], 
+[20, 29], [30, 39], [40, 49], [50, 59] [60, 69], [70, 79], [80, 999].
 - Quantidade de óbitos para RT-PCR positivos.
 
 Com base nessas informações é possível analisar graficamente quais faixas etárias estão testando positivo para o novo coronavírus.
@@ -41,7 +56,6 @@ elif option == "Rio Grande do Sul":
 df.replace(np.nan, 0, inplace=True)
 df['Data'] = pd.to_datetime(df['Data'], format="%Y-%m-%d")
 
-
 st.subheader("Deseja filtrar por algum período específico?")
 d3 = st.date_input("Período selecionado", [])
 if len(d3) > 0:
@@ -49,23 +63,39 @@ if len(d3) > 0:
     df = df[df['Data'] <= str(d3[1])]
     st.write("Início: {} | Fim: {}".format(str(d3[0]), str(d3[1])))
 
+st.subheader("Deseja filtrar por alguma faixa etária?")
+for k in FAIXA_ETARIA:
+    FAIXA_ETARIA[k][0] = st.checkbox(k)
+
+filtro_idades = []
+for k in FAIXA_ETARIA:
+    if FAIXA_ETARIA[k][0]:
+        filtro_idades.append(FAIXA_ETARIA[k][1])
+
+df['pcr'] = df['pcr-positivo'] + df['pcr-negativo']
 df = df[df['pcr-positivo'] != 0]
 ignore_columns = [
+    'pcr',
     'pcr-positivo',
     'pcr-negativo',
     'Data',
     'obitos'
 ]
+df = df[ignore_columns + filtro_idades]
 st.write(df.head())
 drop_columns = []
 plot_columns = []
+## Percentual de positividade por faixa etária
 for c in df.columns.values:
     if not(c in ignore_columns):
         df[c + "_percent"] = ((df[c]/df['pcr-positivo'])*100).round(3)
         drop_columns.append(c)
         plot_columns.append(c + "_percent")
-df.drop(drop_columns, axis=1, inplace=True)
-
+## Percentual de positividade por teste realizado
+df["positividade"] = ((df['pcr-positivo'] / df['pcr']) * 100).round(3)
 df.set_index('Data', inplace=True)
-st.subheader("Gráfico gerado")
+st.subheader("Gráfico de positivos (%) por faixa etária")
 st.area_chart(df[plot_columns])
+
+st.subheader("Gráfico de positividade (%)")
+st.area_chart(df['positividade'])
